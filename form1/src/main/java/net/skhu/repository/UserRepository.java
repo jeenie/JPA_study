82698b2f0be3dcae7efb9a825b7c9fb2274cc82e
jeenie <jeenie828@gmail.com>
2018-12-20 01:57:10 +0900

    spring form validation

---
+++ form1/src/main/java/net/skhu/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package net.skhu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import net.skhu.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
